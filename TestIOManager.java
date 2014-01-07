e2003030ce1a2cb6ab866b3255abf1567ca7d4da
Sam Brown <sbrown5827@gmail.com>
2014-01-07 23:55:18 +0000

    Wrote unit tests for the IOManager class. Empty text file to test exception.

--- TestIOManager.java
+++ TestIOManager.java
@@ -4,19 +4,34 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestIOManager
-{
+import phonetic_search.IOManager;
+import phonetic_search.Surname;
+
+public class TestIOManager {
 
 	private IOManager fi;
 	private File f;
 	private ArrayList<Surname> names;
+	private static final ArrayList<String> expectedMatches = new ArrayList<String>();
+	
+	@BeforeClass
+	public static void initClassStart()
+	{
+		expectedMatches.add("Smith");
+		expectedMatches.add("Smyth");
+		expectedMatches.add("Smythe");
+		expectedMatches.add("Smid");
+		expectedMatches.add("Schmidt");
+	}
 	
 	@Before
 	public void initialise() throws IOException
-	{
+	{	
 		// create new file object from surnames text file
 		f = new File("surnames.txt");
 		// create new file input object from file
@@ -41,45 +56,60 @@ public class TestIOManager
 		names.add(new Surname("Van Damme"));
 	}
 	
-	@Test
-	public void testGetArgs()
+	@After
+	public void teardown()
 	{
-		// TODO: Complete of the getArgs method
-		// How to simulate the buffered reader...?
+		fi = null;
+		f = null;
+	}
+	
+	@Test(expected=IOException.class)
+	public void testConstructorIOException() throws IOException
+	{
+		// should throw exception
+		fi = new IOManager(new File("Rubbish.txt"));
 	}
 	
 	@Test
 	public void testFindMatches()
 	{
-		// TODO: Complete test for the findMatches method
-		// MAY NOT BE NECESSARY AS IT IS MORE OF SYSTEMS TEST TO
-		// MAKE SURE IT PRINTS TO CMD CORRECTLY...
+		// preprocess the surnames
+		Matching.preprocessList(names); // already tested in TestMatching
+		// weight the surnames
+		Matching.applyWeights(names); // already tested in TestMatching
+		// set nameList to equal this
+		fi.nameList = names;
+		// find the matches
+		fi.findMatches("Smith");
+		// check the matches are correct
+
+		assertEquals(expectedMatches, fi.matches);
 	}
 	
 	@Test
-	public void testParseNameFile()
+	public void testParseNameFileNotEmpty() throws IOException, EmptyTextFileException
 	{
-		// TODO: Complete test for the parseNameFile method
-		// test to see if the nameList contains the correct original
-		// surnames, preprocessList and applyWeights methods are tested
-		// in the TestMatching class.
+		// perform the parseNameFile method
+		fi.parseNameFile();
+		int i = 0; // counter
+		// check the namelist variable to see if it surnameOriginal is correct
+		for (Surname s : fi.nameList)
+		{
+			// check the surname in the array list of surnames generated from the file
+			// is the one expected
+			assertEquals(names.get(i).getSurnameOriginal(), s.getSurnameOriginal());
+			i++;
+		}
 	}
 	
-//	@Test
-//	public void testParseLine() throws IOException {
-//		// test can only be run if the parseNameFile method in the IOManager
-//		// class is set to be public static. This was done but changed to accomodate
-//		// implementation changes.
-//		fi.parseNameFile();
-//		ArrayList<Surname> actual = fi.getNameList();
-//		assertEquals(names.size(), actual.size());
-//		for (int i = 0; i<actual.size(); i++)
-//		{
-//			assertEquals(names.get(i).getSurnameOriginal(),actual.get(i).getSurnameOriginal());
-//		}
-//		
-//	}
-	
-	
-
+	@Test(expected=EmptyTextFileException.class)
+	public void testParseNameFileEmpty() throws IOException, EmptyTextFileException
+	{
+		// create new file object from surnames text file
+		f = new File("empty.txt");
+		// create new file input object from file
+		fi = new IOManager(f);
+		// perform the parseNameFile method, should throw exception
+		fi.parseNameFile();
+	}
 }
